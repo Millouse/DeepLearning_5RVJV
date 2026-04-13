1e4a218896ad8b7e3b98b00d61c9a65723083a51
comeprz <come.przybyszewski@gmail.com>
2026-04-13 21:45:12 +0200

    UI Macroquad

--- src/main.rs
+++ src/main.rs
@@ -93,10 +93,22 @@ fn random_vs_mcts_tictactoe() {
     }
 }
 
-fn main() {
-    // lineworld_and_random_agent();
-    // gridworld_and_random_agent();
-    // gridworld_and_randomrollout_agent();
-    // random_vs_random_tictactoe();
-    random_vs_mcts_tictactoe();
+use macroquad::prelude::*;
+
+#[macroquad::main("MyGame")]
+async fn main() {
+    loop {
+        // lineworld_and_random_agent();
+        // gridworld_and_random_agent();
+        // gridworld_and_randomrollout_agent();
+        // random_vs_random_tictactoe();
+        //random_vs_mcts_tictactoe();
+
+        clear_background(RED);
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        draw_text("Hello, Macroquad!", 20.0, 20.0, 30.0, DARKGRAY);
+        next_frame().await }
 }
+
+
