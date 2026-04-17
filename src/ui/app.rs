196b2854deced933fc215905a40fdffcae3528f7
comeprz <come.przybyszewski@gmail.com>
2026-04-17 19:49:38 +0200

    Debut board

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -1,3 +1,4 @@
+use macroquad::miniquad::window::set_window_size;
 use macroquad::prelude::*;
 use crate::ui::assets::load_assets;
 use crate::ui::render::draw_board;
@@ -6,6 +7,7 @@ pub async fn run_ui() {
     let assets = load_assets().await;
     
     loop {
+        set_window_size(1280, 720);
         draw_board(&assets);
 
         next_frame().await;
--- src/ui/render.rs
+++ src/ui/render.rs
@@ -1,3 +1,4 @@
+use macroquad::miniquad::window::set_window_size;
 use macroquad::prelude::*;
 use crate::ui::assets::Assets;
 
@@ -5,7 +6,12 @@ pub fn draw_background(assets: &Assets){
     draw_texture(&assets.background, 0.0, 0.0, WHITE);
 }
 pub fn draw_board(assets: &Assets) {
-    clear_background(LIGHTGRAY);
     draw_background(assets);
+
     draw_text("Pond UI", 20.0, 40.0, 40.0, WHITE);
+
+    draw_rectangle(screen_width() / 2.0 - 200.0, 150.0, 400.0, 100.0, WHITE);
+    draw_rectangle(screen_width() / 2.0 - 200.0, 250.0, 400.0, 100.0, WHITE);
+    draw_rectangle(screen_width() / 2.0 - 200.0, 350.0, 400.0, 100.0, WHITE);
+    draw_rectangle(screen_width() / 2.0 - 200.0, 450.0, 400.0, 100.0, WHITE);
 }
