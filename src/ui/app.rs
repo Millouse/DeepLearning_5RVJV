becc804a8f2b43fb1fff650de51ab740ae1d572f
comeprz <come.przybyszewski@gmail.com>
2026-04-19 17:54:39 +0200

    Click & hover

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -1,15 +1,26 @@
 use macroquad::miniquad::window::set_window_size;
 use macroquad::prelude::*;
 use crate::ui::assets::load_assets;
-use crate::ui::render::draw_board;
+use crate::ui::render::{draw_board};
+use crate::ui::input::{losange_clicked, losange_hovered};
 
 pub async fn run_ui() {
     let assets = load_assets().await;
+    let mut selected_losange: Option<i32> = None;
     
     loop {
         set_window_size(1280, 720);
         draw_board(&assets);
 
+        let board_x= screen_width() / 2.0 - 250.0;
+        let board_y= screen_height() / 2.0 - 250.0;
+        let cell_size = 100.0;
+        if let Some(id) = losange_clicked(board_x, board_y, cell_size){
+            selected_losange = Some(id);
+            println!("Selected losange clicked {}", id);
+        }
+        
+        losange_hovered(board_x, board_y, cell_size);
         next_frame().await;
     }
 }
---
+++ src/ui/input.rs
@@ -0,0 +1,40 @@
+use macroquad::prelude::*;
+use crate::ui::render::board_positions;
+
+pub fn losange_clicked(board_x: f32, board_y: f32, cell_size: f32) -> Option<i32> {
+    if !is_mouse_button_pressed(MouseButton::Left) {
+        return None
+    }
+
+    else{
+        let (mouse_x, mouse_y) = mouse_position();
+        let losanges = board_positions(board_x, board_y, cell_size);
+
+        for losange in losanges{
+            let delta_x = mouse_x - losange.screen_x;
+            let delta_y = mouse_y - losange.screen_y;
+            let distance = (delta_x * delta_x + delta_y * delta_y).sqrt();
+
+            if distance <= 50.0{
+                return Some(losange.id);
+            }
+        }
+
+        None
+    }
+}
+
+pub fn losange_hovered(board_x: f32, board_y: f32, cell_size: f32)  {
+    let losanges = board_positions(board_x, board_y, cell_size);
+
+    let (mouse_x, mouse_y) = mouse_position();
+    for losange in losanges{
+        let delta_x = mouse_x - losange.screen_x;
+        let delta_y = mouse_y - losange.screen_y;
+        let distance = (delta_x * delta_x + delta_y * delta_y).sqrt();
+
+        if distance <= 50.0{
+            draw_rectangle(losange.screen_x - 50.0, losange.screen_y - 50.0, cell_size, cell_size, Color::from_rgba(0, 255, 255, 128));
+        }
+    }
+}
